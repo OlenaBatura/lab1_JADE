c6edfbe8863d99cad0b8224ae8f6ad350990272a
Olena_Batura <Olena_Batura@epam.com>
2016-11-10 21:29:42 +0200

    Jade Project Creation

--- jade/src/jade/core/agent/BookBuyerAgent.java
+++ jade/src/jade/core/agent/BookBuyerAgent.java
@@ -1,11 +1,25 @@
 package jade.core.agent;
 
+import jade.core.AID;
 import jade.core.Agent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by USER on 31.10.2016.
+ * Created by Olena_Batura on 31.10.2016.
  */
 public class BookBuyerAgent extends Agent {
+
+    private String targetBookTicket;
+
+    private List<AID> sellerAgent = new ArrayList<AID>(){{
+        add(new AID("seller1",AID.ISLOCALNAME));
+        add(new AID("seller2",AID.ISLOCALNAME));
+        add(new AID("seller3",AID.ISLOCALNAME));
+    }};
+
+
     protected void setUp(){
         System.out.format("Hello! Buyer-agent {0} is ready!", getAID().getName());
     }
